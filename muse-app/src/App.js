b8981a8cedad248aac3e5de1cbb1f533ad18f2a5
Leon <t7oele00@students.oamk.fi>
2021-05-06 16:27:44 +0300

    Audio reference, event handlers

--- muse-app/src/App.js
+++ muse-app/src/App.js
@@ -8,11 +8,15 @@ function App() {
 
   const [songs, setSongs] = useState(data());
   const [currentSong,  setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player/>
+      <Player 
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying} 
+        currentSong={currentSong}/>
     </div>
   );
 }
